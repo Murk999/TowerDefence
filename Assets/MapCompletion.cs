e2f047c1bc13d130b22f6359effec5b7b946ffde
Murk999 <limonadik34rus@gmail.com>
2024-12-19 14:58:54 +0300

    add save

--- Assets/MapCompletion.cs
+++ Assets/MapCompletion.cs
@@ -6,18 +6,40 @@ namespace TowerDefense
 {
     public class MapCompletion : MonoSingleton<MapCompletion>
     {
+        const string filename = "completion.dat";
         [Serializable]
         private class EpisodeScore
         {
             public Episode episode;
             public int score;
         }
+        
         public static void SaveEpisodeResult(int levelScore)
         {
             Instance.SaveResult(LevelSequenceController.Instance.CurrentEpisode, levelScore);
         }
-
+        private void SaveResult(Episode CurrentEpisode, int levelScore)
+        {
+            foreach (var item in completionData)
+            {
+                if (item.episode == CurrentEpisode)
+                {
+                    if (levelScore > item.score)
+                    {
+                        item.score = levelScore;
+                        Saver<EpisodeScore[]>.Save(filename, completionData);
+                    }
+                }
+            }
+        }
         [SerializeField] private EpisodeScore[] completionData;
+
+        private new void Awake()
+        {
+            base.Awake();
+            Saver<EpisodeScore[]>.TryLoad(filename, ref completionData);
+        }
+
         public bool TryIndex(int id, out Episode episode, out int score)
         {
             if(id >= 0 && id < completionData.Length)
@@ -29,17 +51,6 @@ namespace TowerDefense
             episode = null;
             score = 0;
             return false;
-        }
-
-        private void SaveResult(Episode CurrentEpisode, int levelScore)
-        {
-            foreach(var item in completionData)
-            {
-                if(item.episode == CurrentEpisode)
-                {
-                    if (levelScore > item.score) item.score = levelScore;
-                }
-            }
         }
     }
 }
---
+++ Assets/Saver.cs
@@ -0,0 +1,39 @@
+﻿using SpaceShooter;
+using System;
+using System.IO;
+using UnityEngine;
+
+namespace TowerDefense
+{
+    [Serializable]
+    internal class Saver<T>
+    {
+        private static string Path(string filename)
+        {
+            return $"{Application.persistentDataPath}/{filename}";
+        }
+        public static void TryLoad(string filename, ref T data)
+        {
+            var path = Path(filename);
+            if (File.Exists(path))
+            {
+                Debug.Log($"loading from{path}");
+                var dataString = File.ReadAllText(path);
+                var saver = JsonUtility.FromJson<Saver<T>>(dataString);
+                data = saver.data;
+            }
+            else
+            {
+                Debug.Log($"no file at{path}");
+            }
+        }
+
+        public static void Save(string filename, T data)
+        {
+            var wrapper = new Saver<T> { data = data }; // врапер оборачиватель
+            var dataString = JsonUtility.ToJson(wrapper);
+            File.WriteAllText(Path(filename), dataString);
+        }
+        public T data;
+    }
+}
